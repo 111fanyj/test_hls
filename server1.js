aaea49c2e0c513a5ba4b826b2d941ce09401018a
FlanReazy <fffanyuji@163.com>
2024-05-28 10:41:40 +0800

    feat handle option

--- server1.js
+++ server1.js
@@ -7,16 +7,22 @@ const PORT = 8001;
 const VIDEO_PATH = './videos/fraBox.mp4'; // 替换为你的视频文件路径
 
 http.createServer((req, res) => {
-    console.log(req.headers.range);
+    console.log(req.headers);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Expose-Headers','Content-Range');
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,responsetype');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,responsetype,ano_cus');
 
 
     const videoStat = fs.statSync(VIDEO_PATH);
     const fileSize = videoStat.size;
     const range = req.headers.range;
+    if (req.method === 'OPTIONS') {
+        // 对预检请求返回 200 状态码，并添加预检请求所需的头部
+        res.writeHead(200);
+        res.end();
+        return;
+      }
 
     if (range) {
         const parts = range.replace(/bytes=/, "").split("-");
@@ -42,7 +48,7 @@ http.createServer((req, res) => {
             'Content-Type': 'video/mp4',
         };
 
-        res.writeHead(200, headers);
+        res.writeHead(400, headers);
         fs.createReadStream(VIDEO_PATH).pipe(res);
     }
 }).listen(PORT,'0.0.0.0', () => {
